dc5796f4803d835aeda0288c1595fcbe86706416
Gabriel Moya <gabrielmoya123@gmail.com>
2024-01-25 11:08:53 -0300

    Add server.js and package.json files

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/health-check', (_, res) => {
+  const healthchek = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now()
+  };
+
+  try {
+    res.send(healthchek);
+  } catch (error) {
+    healthchek.message = error;
+    res.status(503).send();
+  }
+});
+
+app.get('/', (_, res) => {
+  res.send('Hello V0.0.1');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
